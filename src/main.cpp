90dde5ea57a2a2fd603ee5e2a8aaed0a8524ce26
aleconte <aleconte@student.42.fr>
2022-01-25 12:11:04 +0100

    feat: use vector to store clients socket for dynamic purpose

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,15 @@
 #include <unistd.h>
 #include "Server.hpp"
 #include <poll.h>
+#include <vector>
 
-#define CLOSED -1
 
-//struct pollfd *enlarge(struct)
-{
-	struct pollfd *fds_new;
-}
+//struct pollfd *enlarge(struct pollfds *fds)
+//{
+//	struct pollfd *fds_new;
+		
+
+//}
 
 int main(void) {
 	Server server;
@@ -42,58 +44,43 @@ int main(void) {
 	socklen_t csize = sizeof(addrClient);
 
 	int	socketClient;
-	int nb_clients = 1;
-	int max_clients = 5;
-	struct pollfd fds[5];
-	//struct pollfd *fds;
-	//fds = new fds[max_clients];
-	for (int i = 0; i < max_clients; i++) fds[i].fd = -1;
-	fds[0].fd = socketServer;
-	fds[0].events = POLLIN;
+	struct pollfd pfd;
+	std::vector<struct pollfd> fds;
+	pfd.fd = socketServer;
+	pfd.events = POLLIN;
 	char buf[19 + 1];
 //	int timeout = 5000;
+	fds.push_back(pfd);
 
-	while (1)
+	while (true)
 	{
-		//if(nb_client + 1 == max_clients)
-		//{
-			//enlarge_fds();
-			//continue;
-
-	//	}
-		if(poll(fds, nb_clients, -1) != -1)
+		if(poll(&fds[0], fds.size(), -1) != -1)	// TODO: add timeout ?
 		{
 			if (fds[0].revents & POLLIN)
 			{
 				socketClient = accept(socketServer, (struct sockaddr *)&addrClient, &csize);
-				if (nb_clients < max_clients)	// TODO: augmenter le tableau
-				{
-					std::cout << "New client " << socketClient << " arrived" << std::endl;
-					fds[nb_clients].fd = socketClient;
-					fds[nb_clients].events = POLLIN | POLLRDHUP | POLLERR;
-					nb_clients++;
-					continue ;
-				}
-				else
-					std::cerr << "max buffer size reached: cannot accept more clients" << std::endl;
+				std::cout << "New client " << socketClient << " arrived" << std::endl;
+				pfd.fd = socketClient;
+				pfd.events = POLLIN | POLLRDHUP | POLLERR;
+				fds.push_back(pfd);
+				continue ;
 			}
 
-			for (int i = 1; i < nb_clients; i++)
+			for (std::vector<struct pollfd>::iterator it = fds.begin()++; it != fds.end(); it++)
 			{
-				if (fds[i].fd != CLOSED && fds[i].revents == POLLIN)
+				if (it->revents == POLLIN)
 				{
 					bzero(buf, 20);	// TODO: remove forbidden func
-					if (recv(fds[i].fd, buf, std::string(buf).length() - 1, 0) <= 0)
+					if (recv(it->fd, buf, std::string(buf).length() - 1, 0) <= 0)
 						std::cerr << "error recv" << std::endl;
-					std::cout << "Client " << fds[i].fd << " says: " << buf;
+					std::cout << "Client " << it->fd << " says: " << buf;
 				}
-				else if (fds[i].fd != CLOSED && fds[i].revents)
+				else if (it->revents)
 				{
-					std::cout << "client " << fds[i].fd << " connection closed" << std::endl;
-					close(fds[i].fd);
-					fds[i].fd = CLOSED;
-					fds[i].events = 0;
-					fds[i].revents = 0;
+					std::cout << "client " << it->fd << " connection closed" << std::endl;
+					close(it->fd);
+					fds.erase(it);
+					break ;
 				}
 				//if ((fds[0].revents | POLLOUT) == POLLOUT)
 				//	send();
